30e6f55cb0eafcb0d5d1f96e6a3cf7853aba858e
AzurDelta <Rotar.zahar31@gmail.com>
2024-06-17 16:11:14 +0300

    task done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,98 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Text;
+Stopwatch sw = Stopwatch.StartNew();
+
+var result = ReturnMinDecimals(8971324560);
+
+Console.WriteLine(result.Item1);
+foreach (var item in result.Item2)
+{
+    Console.Write($"{item} ");
+}
+
+sw.Stop();
+
+Console.WriteLine($"\n{sw.Elapsed}");
+
+sw.Restart();
+
+PrintMinDecimals(828283754998);//actually 6 times faster
+
+Console.WriteLine(sw.Elapsed);
+
+(int, List<long>) ReturnMinDecimals(long number)
+{
+    int len;
+    List<long> outNumbers = new();
+    int count = 0;
+    var numberAsString = "";
+    StringBuilder stringBuilder = new();
+    for (var i = 9; i > 0; i--)
+    {
+        if (number.ToString().Contains($"{i}"))//looking for biggest number, if not a current i - skip
+        {
+            numberAsString = number.ToString();
+            //Console.Write(number + "   ");//debug
+        
+            foreach (var ch in numberAsString)//mask of 01 for number substraction
+            {
+                if (ch == $"{i}".ToArray()[0])
+                    stringBuilder.Append('1');
+                else
+                    stringBuilder.Append('0');
+            }
+            
+            outNumbers.Add(long.Parse(stringBuilder.ToString()));//add mask to output (needed)
+            number -= long.Parse(stringBuilder.ToString()); //decrease input number to cycle further
+            count++;//counter of numbers (needed)
+
+            //Console.WriteLine(number); //debug
+        }
+
+        stringBuilder.Clear();
+    }
+
+    return (count, outNumbers);
+}
+
+
+void PrintMinDecimals(long number)
+{
+    int len;
+    List<long> outNumbers = new();
+    int count = 0;
+    var numberAsString = "";
+    StringBuilder stringBuilder = new();
+    for (var i = 9; i > 0; i--)
+    {
+        if (number.ToString().Contains($"{i}"))//looking for biggest number, if not a current i - skip
+        {
+            numberAsString = number.ToString();
+            //Console.Write(number + "   ");//debug
+
+            foreach (var ch in numberAsString)//mask of 01 for number substraction
+            {
+                if (ch == $"{i}".ToArray()[0])
+                    stringBuilder.Append('1');
+                else
+                    stringBuilder.Append('0');
+            }
+
+            outNumbers.Add(long.Parse(stringBuilder.ToString()));//add mask to output (needed)
+            number -= long.Parse(stringBuilder.ToString()); //decrease input number to cycle further
+            count++;//counter of numbers (needed)
+
+            //Console.WriteLine(number); //debug
+        }
+
+        stringBuilder.Clear();
+    }
+
+    Console.Write($"{count}; "); //output section
+    
+    for (int i = 0; i < outNumbers.Count - 2; i++)
+    {
+        Console.Write($"{outNumbers[i]}, ");
+    }
+    Console.WriteLine(outNumbers[outNumbers.Count - 1]);
+}
